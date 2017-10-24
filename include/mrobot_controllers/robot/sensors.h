451272ea4c14421f2f54e9b76c0b45015b3fbbb6
Unknown <inho211@naver.com>
2017-10-25 03:42:20 +0900

    parse_cloud function changed
    pharse_cloud -> parse_cloud
    context updated (use sensor number 0~7)

--- include/mrobot_controllers/robot/sensors.h
+++ include/mrobot_controllers/robot/sensors.h
@@ -3,7 +3,8 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <sensor_msgs/PointCloud2.h>
-
+#define L_RES 0.02 //line resolution
+#define NUM_IR_PTS 7 //number of ir points
 namespace mrobot_control
 {
 	class laser_sensor
@@ -11,10 +12,11 @@ namespace mrobot_control
 		public:
 			laser_sensor();
 			~laser_sensor();
-			void pharse_cloud(pcl::PointCloud<pcl::PointXYZI>& inCloud);
+			void parse_cloud(pcl::PointCloud<pcl::PointXYZI>& inCloud);
 			sensor_msgs::PointCloud2& get_cloud() {return outCloud;}
 		private:
 			pcl::PointCloud<pcl::PointXYZI> phCloud; //Pharsed Cloud
 			sensor_msgs::PointCloud2 outCloud; //for checking
+			pcl::PointXYZI ir_points[NUM_IR_PTS]; //7 points
 	};
 }
--- src/sensors.cpp
+++ src/sensors.cpp
@@ -5,37 +5,37 @@ namespace mrobot_control
 {
 	laser_sensor::laser_sensor(void){}
 	laser_sensor::~laser_sensor(void){}
-	void laser_sensor::pharse_cloud(pcl::PointCloud<pcl::PointXYZI>& inCloud)
+	void laser_sensor::parse_cloud(pcl::PointCloud<pcl::PointXYZI>& inCloud)
 	{
-		phCloud.clear(); //clear points
+		phCloud.clear(); //clear phCloud
+		for(int i = 0 ; i < NUM_IR_PTS; i++){
+			ir_points[i].x = 0; ir_points[i].y = 0; ir_points[i].z = 0; ir_points[i].intensity = 0;
+		}
 		//printf("phCloud.points.size() = %d\n",phCloud.points.size());
 		phCloud.header = inCloud.header;
-		pcl::PointCloud<pcl::PointXYZI>::iterator p = inCloud.begin();
-		pcl::PointCloud<pcl::PointXYZI>::iterator p2 = phCloud.begin();
-		pcl::PointXYZI temp;
+		pcl::PointCloud<pcl::PointXYZI>::iterator pts_in = inCloud.begin();
+		pcl::PointCloud<pcl::PointXYZI>::iterator pts_out = phCloud.begin();
+		pcl::PointXYZI p;
 
-		while(p != inCloud.end()){//points downsampling
-			float x = p->x; 
-			float y = p->y; 
-			float z = p->z;
-			if(y >= 0){ //get 6 points
-				if(y < 0.02){ // y = 0.02
-					temp.x = x; temp.y = y; temp.z = z; temp.intensity= p->intensity;
-					p2 = phCloud.insert(p2,temp);
-				}else if(y - x >= -0.02 && y - x <= 0.02){ // y = x
-					temp.x = x; temp.y = y; temp.z = z; temp.intensity= p->intensity;
-					p2 = phCloud.insert(p2,temp);
-				}else if(y + x >= -0.02 && y + x <= 0.02){ //y = -x
-					temp.x = x; temp.y = y; temp.z = z; temp.intensity= p->intensity;
-					p2 = phCloud.insert(p2,temp);
-				}else if(x >= -0.02 && x <= 0.02){
-					temp.x = x; temp.y = y; temp.z = z; temp.intensity= p->intensity;
-					p2 = phCloud.insert(p2,temp);
-				}
-			}else{
-				;
+		while(pts_in != inCloud.end()){ //get ir like data
+			p.x = pts_in->x; p.y = pts_in->y; p.z = pts_in->z; p.intensity = pts_in->intensity;
+			if(p.x >= -L_RES && p.x <= L_RES){		// LINE RESOLUTION = 0.02 (meter)
+				if(p.y >= 0) ir_points[1] = p;
+				else ir_points[5] = p;
+				pts_out = phCloud.insert(pts_out,p);	//for checking in rviz
+			}else if(p.y - p.x >= -L_RES && p.y - p.x <= L_RES){ // y = x
+				if(p.x >= 0) ir_points[2] = p;
+				else ir_points[6] = p;
+				pts_out = phCloud.insert(pts_out,p); 
+			}else if(p.y + p.x >= -L_RES && p.y + p.x <= L_RES){ //y = -x
+				if(p.x >= 0) ir_points[4] = p;
+				else ir_points[0] = p;
+				pts_out = phCloud.insert(pts_out,p);
+			}else if(p.y >= -L_RES && p.y <= L_RES){
+				if(p.x >= 0) ir_points[3] = p;
+				pts_out = phCloud.insert(pts_out,p);
 			}
-			p++;
+			pts_in++;
 		}
 		pcl::toROSMsg(phCloud, outCloud);
 	}
